ed41ecc0aa9d1197881c47faa620e92f76776be1
ryuto94 <ryutoguti@hotmail.com>
2019-08-01 16:40:22 -0300

    Add API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/usuarios', "api\UsuariosController@pegarTodos");
+Route::get('/usuarios/{id}','api\UsuariosController@pegarUm');
+
+Route::post('/usuarios', "api\UsuariosController@criarUm");
+
+Route::delete('/usuarios/{id}', "api\UsuariosController@deletarUm");
+
+Route::put('/usuarios/{id}', "api\UsuariosController@alterarUm");
